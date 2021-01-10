ca77ef6f653623212ddf03751f13dd33c640fb04
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2021-01-10 18:57:32 +0700

    add authorization to API

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,13 +1,17 @@
 import db from '../../../libs/db';
+import authorization from '../../../middlewares/authorization';
 
 export default async function handler(req, res) {
     if(req.method !== 'GET') return res.status(405).end();
 
-    const posts = await db('posts');
+        const auth = await authorization(req, res);
+        const posts = await db('posts');
+    
+        res.status(200);
+        res.json({
+            message: 'Posts data',
+            data: posts
+        });
 
-    res.status(200);
-    res.json({
-        message: 'Posts data',
-        data: posts
-    });
+
 }
